997f44387484a6a13a3cd9f7324f5b4a46a90553
mshalini3111 <mshalini3111@gmail.com>
2026-03-03 10:24:56 +0530

    UC2 Banner

--- OOPSBannerUC2.java
+++ OOPSBannerUC2.java
@@ -1,15 +1,13 @@
 public class OOPSBannerUC2 {
-    public static void main(String[] args) {
-
-
-                        System.out.println(" **    **   **    *** ");
-                        System.out.println("*     *  *     *  *     *  *     *");
-                        System.out.println("*     *  *     *  *     *  *      ");
-                        System.out.println("*     *  *     *  **    *** ");
-                        System.out.println("*     *  *     *  *              *");
-                        System.out.println("*     *  *     *  *        *     *");
-                        System.out.println(" **    **   *         *** ");
-
-
+    public static void main(String[] args){
+        System.out.println("   ***    " + "   ***   " + " ******    " + "   ***** ");
+        System.out.println(" **   **  " + " **   ** " + " **    **  " + " **      ");
+        System.out.println("**     ** " + "**     **" + " **     ** " + "**       ");
+        System.out.println("**     ** " + "**     **" + " **    **  " + " **      ");
+        System.out.println("**     ** " + "**     **" + " ******    " + "   ***   ");
+        System.out.println("**     ** " + "**     **" + " **        " + "      ** ");
+        System.out.println("**     ** " + "**     **" + " **        " + "       **");
+        System.out.println(" **   **  " + " **   ** " + " **        " + "      ** ");
+        System.out.println("   ***    " + "   ***   " + " **        " + " *****  ");
     }
 }
